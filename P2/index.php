da3238fa09b8fa15d6e0ffe460a16f7c6c4b38e9
Daniel Ranchal <danitico98@gmail.com>
2018-09-29 12:54:01 +0200

    Practica 2 terminada

--- P2/index.php
+++ P2/index.php
@@ -1,10 +1,45 @@
-<?php
- echo "<!DOCTYPE html>\n<html>";
- echo "<head>\n<meta charset=\"utf-8\">\n<title>Listado de empleados</title>\n</head>";
- echo "<body>\n\t<table border=\"2\">";
- echo "<tr>\n<th>Número de empleado</th>\n<th>Nombre del empleado</th>\n</tr>";
- echo "<tr>\n<td>1</td>\n<td>\n<a href=\"#\">Juan Pacheco</a></td></tr>";
- echo "<tr>\n<td>1</td>\n<td>\n<a href=\"#\">Francisco Ruiz</a></td></tr>";
- echo "<tr>\n<td>1</td>\n<td>\n<a href=\"#\">Aurelio Martos</a></td></tr>";
- echo "</table></body>\n</html>";
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Listado de empleados</title>
+    </head>
+    <body>
+        <table border="2">
+            <tr>
+                <th>Número de empleado</th>
+                <th>Nombre del empleado</th>
+            </tr>
 
+            <?php
+                $SERVERNAME="oraclepr.uco.es";
+                $USERNAME="i62rapad";
+                $PASSWORD="InGeNiErO1";
+                $DATABASE="i62rapad";
+
+                $db = new mysqli($SERVERNAME, $USERNAME, $PASSWORD, $DATABASE);
+
+                if($db->connect_error){
+                    die('Connect Error (' . $db->connect_errno . ') ' . $db->connect_error);
+                }
+
+
+                $query = "SELECT NOMBRE FROM EMPLEADOS";
+                $results = $db->query($query);
+                $results = $results->fetch_all(MYSQLI_NUM);
+                $i = 1;
+
+                foreach ($results as $name){
+                    echo "<tr>\n<td>$i</td>\n";
+                    echo "<td>\n";
+                    echo '<a href="details.php?NOMBRE=' . urlencode($name[0]) . '">';
+                    echo $name[0] . '</a>';
+                    echo "</td></tr>";
+                    $i++;
+                }
+
+                $db->close();
+            ?>
+        </table>
+    </body>
+</html>
